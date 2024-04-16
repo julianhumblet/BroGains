6e84b16856beab2d64b0233f95afce60d83be932
Humblet,Julian J.M.M <julian.humblet@student.fontys.nl>
2024-04-16 23:50:33 +0200

    Modified hashing functionality

--- hashing/hashing.go
+++ hashing/hashing.go
@@ -2,10 +2,23 @@ package hashing
 
 import (
 	"encoding/base64"
+	"fmt"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
+
+func HashPassword(password string) (string, error) {
+
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return "", fmt.Errorf("error hashing password: %s", err)
+	}
+
+	hashedPassword := base64.StdEncoding.EncodeToString(hashedBytes)
+
+	return hashedPassword, nil
+}
 
 func CheckHashPassword(password, hash string) bool {
 
