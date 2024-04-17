194ca6aa06b319a143245ab824728dabebb29d1c
Humblet,Julian J.M.M <julian.humblet@student.fontys.nl>
2024-04-17 02:28:39 +0200

    Modified webserver functionality

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -8,7 +8,12 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/sessions"
 )
+
+var store *sessions.CookieStore
+var sessionName string
 
 // Constant values
 const (
@@ -22,17 +27,44 @@ const (
 	usernameExists     = "Username is already in use"
 )
 
+// Function to assign the session info to the package level variables
+func SetupSessions() {
+
+	sessionName = configfile.Configuration.Webserver.SessionName
+
+	store = sessions.NewCookieStore([]byte(
+		configfile.Configuration.Webserver.SecretSessionKey,
+	))
+}
+
 func StartWebserver(port int) {
 
-	// Serve the static files
-	http.Handle("/",
-		http.FileServer(
-			http.Dir("webserver/static"),
+	// Create the fileservers
+	fileserverLogin := http.FileServer(
+		http.Dir("webserver/static/login"),
+	)
+	fileserverDashboard := http.FileServer(
+		http.Dir("webserver/static/dashboard"),
+	)
+
+	// Host the fileservers
+	http.Handle(
+		"/",
+		fileserverLogin,
+	)
+	http.Handle(
+		"/dashboard/",
+		isAuthenticated(
+			http.StripPrefix(
+				"/dashboard/",
+				fileserverDashboard,
+			),
 		),
 	)
 
 	// Host the handlers
 	http.HandleFunc("/login", loginHandler)
+	http.Handle("/logout", isAuthenticated(http.HandlerFunc(logoutHandler)))
 	http.HandleFunc("/register", registrationHandler)
 
 	// Start the webserver
@@ -107,8 +139,69 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Start a session for the authenticated user
+	session, err := store.Get(r, sessionName)
+	if err != nil {
+		log.Printf("error getting session: %s", err)
+		http.Error(w, internalError, http.StatusInternalServerError)
+		return
+	}
+
+	// Set session value to indicate user is authenticated
+	session.Values["authenticated"] = true
+	session.Values["username"] = user.Username // Store additional user data if needed
+	session.Save(r, w)
+
 	// Send HTTP code 200, login is succesful
 	w.WriteHeader(http.StatusOK)
+
+	// Redirect the user to the dashboard page
+	http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Check if the made request is POST
+	if r.Method != http.MethodPost {
+		http.Error(w, methodNotAllowed, http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Check if the given API key is valid
+	enteredApiKey := r.Header.Get("Authorization")
+	if !isAPIKeyValid(enteredApiKey) {
+		http.Error(w, unauthorized, http.StatusUnauthorized)
+		return
+	}
+
+	// Retrieve the session for the current request
+	session, err := store.Get(r, sessionName)
+	if err != nil {
+		log.Printf("error getting session: %s", err)
+		http.Error(w, internalError, http.StatusInternalServerError)
+		return
+	}
+
+	// Clear the session
+	session.Values["authenticated"] = false
+	session.Values["username"] = nil
+
+	// Set MaxAge to -1 to delete the session cookie after browser close
+	session.Options.MaxAge = -1
+
+	// Save the session
+	err = session.Save(r, w)
+	if err != nil {
+		log.Printf("error saving session: %s", err)
+		http.Error(w, internalError, http.StatusInternalServerError)
+		return
+	}
+
+	// Send HTTP code 200, logout is succesful
+	w.WriteHeader(http.StatusOK)
+
+	// Redirect the user to the login page
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func registrationHandler(w http.ResponseWriter, r *http.Request) {
@@ -176,8 +269,34 @@ func registrationHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Send HTTP code 200, login is succesful
+	// Send HTTP code 200, registration is succesful
 	w.WriteHeader(http.StatusOK)
+
+	// Redirect the user to the login page
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+// Check if the user is logged in
+func isAuthenticated(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Retrieve the session for the current request
+		session, err := store.Get(r, sessionName)
+		if err != nil {
+			log.Printf("error getting session: %s", err)
+			http.Error(w, internalError, http.StatusInternalServerError)
+			return
+		}
+
+		// Check if the user is authenticated
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			http.Error(w, unauthorized, http.StatusUnauthorized)
+			return
+		}
+
+		// Proceed to the next handler if the user is authenticated
+		next.ServeHTTP(w, r)
+	})
 }
 
 // Checking if the given given APIKey is valid
